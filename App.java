1e1326327fcdb215901ec01e4b5b8facaab4d1e4
chayaponsaenthavee <chayapon.sae@ku.th>
2025-07-03 18:01:02 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,7 +1,9 @@
 import lib.*;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        //PasswordValidatorTest.run();
+        //System.out.println("Hello World!!!");
+        PasswordValidatorTest.run();
+         //new PasswordValidator();
+
     }
 }
